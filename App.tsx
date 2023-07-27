861d2b533b11c5f446c6520a14473e0ac6c52616
Italo Leal <itlcoorporativo@gmail.com>
2023-07-27 13:52:14 -0300

    Criação da StatusBar

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import { ActivityIndicator } from 'react-native'
+import { StatusBar } from 'react-native'
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 import theme from './src/theme'
 
 import { Loading } from '@components/Loading'
+
 import { Groups } from '@screens/Groups';
 
 export default function App() {
@@ -14,6 +15,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );

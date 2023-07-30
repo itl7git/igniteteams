8e8eababbf85c6f7095c265291ac429ec06d97a2
Italo Leal <itlcoorporativo@gmail.com>
2023-07-29 23:12:11 -0300

    Corrige os erros de estilização dos components

--- App.tsx
+++ App.tsx
@@ -11,8 +11,6 @@ import { Groups } from '@screens/Groups';
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
-
-
   return (
     <ThemeProvider theme={theme}>
       <StatusBar
--- src/components/Highlight/styles.ts
+++ src/components/Highlight/styles.ts
@@ -9,14 +9,14 @@ export const Title = styled.Text`
   text-align: center;
 
   font-size: ${({ theme }) => theme.FONT_SIZE.XL}px;
-  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD}px;
-  color: ${({ theme }) => theme.COLORS.WHITE}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+  color: ${({ theme }) => theme.COLORS.WHITE};
 `
 
 export const Subtitle = styled.Text`
   text-align: center;
 
   font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
-  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR}px;
-  color: ${({ theme }) => theme.COLORS.GRAY_300}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+  color: ${({ theme }) => theme.COLORS.GRAY_300};
 `

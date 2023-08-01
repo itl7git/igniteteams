bb07fa5ffcedda353d80a905c45d8b00b5c1c40b
Italo Leal <itlcoorporativo@gmail.com>
2023-08-01 13:57:08 -0300

    Implementa a navegação da aplicação por completo: ir e voltar por telas

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,8 +3,15 @@ import { Container, Content, Icon } from './styles'
 import { Highlight } from '@components/Highlight'
 import { Button } from '@components/Button'
 import { Input } from '@components/Input'
+import { useNavigation } from '@react-navigation/native'
 
 export function NewGroup() {
+  const navigation = useNavigation()
+
+  function handleNew() {
+    navigation.navigate('players', { group: 'Rocket' })
+  }
+
   return (
     <Container>
       <Header showBackButton />
@@ -21,6 +28,7 @@ export function NewGroup() {
 
         <Button
           title="Criar"
+          onPress={handleNew}
           style={{ marginTop: 20 }}
         />
       </Content>

96121e192587499d474f726a68a13bdbaedff891
Italo Leal <itlcoorporativo@gmail.com>
2023-08-01 12:29:33 -0300

    Instala a biblioteca react-navigation e implementa a estrutura

--- App.tsx
+++ App.tsx
@@ -10,6 +10,8 @@ import { Groups } from '@screens/Groups';
 import { NewGroup } from '@screens/NewGroup';
 import { Players } from '@screens/Players';
 
+import { Routes } from './src/routes'
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
@@ -20,7 +22,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
-      {fontsLoaded ? <Players /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }

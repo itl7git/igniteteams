61eaf70fd0db850493d84ee2efcf76c26ffbf71d
Italo Leal <itlcoorporativo@gmail.com>
2023-08-06 02:18:07 -0300

    Identificando o foco na tela(usando useFocusEffect e useCallback)

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import { FlatList } from 'react-native';
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useFocusEffect } from '@react-navigation/native'
 
 import { Header } from '@components/Header';
 import { Container } from './styles';
@@ -8,6 +8,7 @@ import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
+import { groupsGetAll } from '@storage/group/groupsGetAll';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>(['Bola da sexta'])
@@ -17,6 +18,19 @@ export function Groups() {
   function handleNewGroup() {
     navigation.navigate('new')
   }
+
+  async function fetchGroups() {
+    try {
+      const data = await groupsGetAll()
+      setGroups(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useFocusEffect(useCallback(() => {
+    fetchGroups()
+  }, []))
 
   return (
     <Container>
